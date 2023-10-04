23949c5fa6f0f90556fe687ddc457b3b1c933495
Iago Moreda <iagomoreda1910@gmail.com>
2023-10-04 19:42:56 +0200

    base plantillas

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,5 +28,38 @@ class RegistrationController extends AbstractController
 
         // ...*/
         return $this->render('registration/index.html.twig', []);
+    }
+
+    #[Route('/registroUsuario', name: 'registro_usuario', methods: ['POST'])]
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher,EntityManagerInterface $entityManager): Response
+    {
+        // Crear una nueva instancia de la entidad User
+        $user = new User();
+
+        // Crear el formulario de registro
+        $form = $this->createForm(RegistrationFormType::class, $user);
+
+        // Procesar el formulario cuando se envía
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // hash the password (based on the security.yaml config for the $user class)
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $form->get('password')->getData()
+            );
+            $user->setPassword($hashedPassword);
+
+            // Guardar el usuario en la base de datos
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // Redirigir a la página de inicio o cualquier otra página deseada después del registro
+            return $this->redirectToRoute('app_index');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
